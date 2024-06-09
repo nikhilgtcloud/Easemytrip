df04489e55c3002aa541a2a03bbaf0c5fbfca6a6
Nikhil Thakur <nikhil.gtcloud@gmail.com>
2024-06-09 17:34:18 +0530

    making some changes

--- src/main/java/easemytrip/example/easemytrip/flight.java
+++ src/main/java/easemytrip/example/easemytrip/flight.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class railway {
-    @GetMapping("/railway")
-    public String getData() {return  "Please book your train ticket on vande bharat 25% discount" ; }
+public class flight {
+    @GetMapping("/flight")
+    public String getData() {return  "Please book your train ticket on vande bharat 45% discount" ; }
 }
