7a10310b6226a52b699cf4903a9250cd0c98acac
Nikhil Thakur <nikhil.gtcloud@gmail.com>
2024-06-11 17:36:34 +0530

    making a change in the hotel file and visa file

--- src/main/java/easemytrip/example/easemytrip/visa.java
+++ src/main/java/easemytrip/example/easemytrip/visa.java
@@ -6,5 +6,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class visa {
     @GetMapping("/visa")
-    public String getData() {return  "Please submit your visa at the embassy." ; }
+    public String getData() {return  "Please submit your visa at the india embassy." ; }
 }
